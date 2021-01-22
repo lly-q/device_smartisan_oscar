e97ae2f9d4b140b0d41174bb9a72566bf241075c
gesangtome <gesangtome@foxmail.com>
2021-01-22 16:20:25 +0800

    msm8953-common: Add vndk sp libraries to system lib.

--- oscar.mk
+++ oscar.mk
@@ -25,3 +25,22 @@ $(call inherit-product-if-exists, vendor/smartisan/oscar/oscar-vendor.mk)
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
+
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hidl.base@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libsync.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    liblzma.vndk-sp
